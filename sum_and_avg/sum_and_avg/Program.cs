62279def8370329f61c7530b74a04c4e9d5a9059
Olesja <rockfun@inbox.lv>
2013-11-18 11:28:10 +0200

    added proga sum and average

---
+++ sum_and_avg/sum_and_avg/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace sum_and_avg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, sum;
+            double av;
+            string s;
+            Console.Write("a="); s = Console.ReadLine();
+            a = int.Parse(s);
+            Console.Write("b="); s = Console.ReadLine();
+            b = int.Parse(s);
+            sum = a + b;
+            av = sum / 2.0;
+            Console.WriteLine("sum={0} av={1}", sum, av);
+            Console.ReadKey();
+        }
+    }
+}
